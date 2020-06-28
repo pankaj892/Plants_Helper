92a87d94173ea25b2503b21ce3e90e0eb5e1b578
msavanan <saravanan.rnd@gmail.com>
2020-06-28 23:29:09 +0530

    homepage

---
+++ android/app/src/main/kotlin/com/example/gardening/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gardening
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:plants/main.dart';
+import 'package:gardening/gardening.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(Gardening());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
